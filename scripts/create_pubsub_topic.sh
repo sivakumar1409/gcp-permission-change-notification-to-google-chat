4d4e75f02952cb06438a4c78ea91e22880b351e1
sivakumars <siva@test.com>
2025-02-26 17:54:54 +0530

    [Siva] | Added logic to delete topic if already exists and recreate;

--- scripts/create_pubsub_topic.sh
+++ scripts/create_pubsub_topic.sh
@@ -7,14 +7,11 @@
 TOPIC_NAME=$PUBSUB_TOPIC_NAME
 
 
-# Check if the topic exists
+# Check if the topic exists, if exists delete it and create new one
 if gcloud pubsub topics describe "$TOPIC_NAME" --project="$PROJECT_ID" > /dev/null 2>&1; then
- echo "Topic $TOPIC_NAME already exists."
-else
- # Create the topic if it does not exist
- gcloud pubsub topics create "$TOPIC_NAME" --project="$PROJECT_ID"
- echo "Topic $TOPIC_NAME created."
+ echo "Topic $TOPIC_NAME already exists, deleting it"
+ gcloud pubsub topics delete "$TOPIC_NAME" --project="$PROJECT_ID"
 fi
+  gcloud pubsub topics create "$TOPIC_NAME" --project="$PROJECT_ID"
+  echo "Topic $TOPIC_NAME created."
 
-
-# gcloud pubsub topics create access_change_logs
