43b61811265463a9dd3a5270d6522f8b97574209
sivakumars <siva@test.com>
2025-02-26 17:51:44 +0530

    [Siva] | Added logic to delete subscription if already exists and recreate;

--- scripts/create_subscription.sh
+++ scripts/create_subscription.sh
@@ -5,9 +5,10 @@
 
 
 if gcloud pubsub subscriptions describe "$PUBSUB_SUBSCRIPTION_NAME" --project="$PROJECT_ID" > /dev/null 2>&1; then
- echo "Subscription $PUBSUB_SUBSCRIPTION_NAME already exists."
-else
-
+  echo "Subscription $PUBSUB_SUBSCRIPTION_NAME already exists. Deleting and recreating..."
+  gcloud pubsub subscriptions delete "$PUBSUB_SUBSCRIPTION_NAME" --project="$PROJECT_ID" --quiet
+  echo "Subscription $PUBSUB_SUBSCRIPTION_NAME deleted."
+fi
 
 # Create the subscription with the Cloud Function as the push endpoint
    gcloud pubsub subscriptions create "$PUBSUB_SUBSCRIPTION_NAME" \
@@ -17,7 +18,5 @@ else
    --push-auth-service-account="$SERVICE_ACCOUNT" \
    --project="$PROJECT_ID"
 
-
    echo "Subscription $PUBSUB_SUBSCRIPTION_NAME created with push endpoint to Cloud Function $HANDLER_GCF_FUNCTION_NAME."
-fi
 
